3f1a3ed84d46f3c9b73c55234df5d35658d474f7
Lucas Terriel <ls.terriel@gmail.com>
2026-07-09 16:15:31 +0200

    update Flask object in app.py - static templates direction

--- app.py
+++ app.py
@@ -102,7 +102,12 @@ def create_app():
         imported by the Flask CLI (``flask --app app run``).
     :rtype: flask.Flask
     """
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        static_url_path='',
+        static_folder='static',
+        template_folder='templates'
+    )
     app.config.from_object(Config)
 
     app.jinja_env.globals["BRANCH_LABELS"] = {
